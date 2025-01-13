3c48556d3a3f154740407236271861d585725449
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-01-13 20:30:53 +0100

    Added docstrings

--- appSecrets.py
+++ appSecrets.py
@@ -1,13 +1,31 @@
 import json
 
 
-def getShowdownUsername():
+def getShowdownUsername() -> str:
+    """
+    Gets the username from the secrets.json file
+
+    Args:
+        - None
+
+    Returns:
+        - str: The username to be used for Showdown
+    """
     with open("secrets.json") as f:
         data = json.load(f)
         return data["username"]
 
 
-def getShowdownPassword():
+def getShowdownPassword() -> str:
+    """
+    Gets the password from the secrets.json file
+
+    Args:
+        - None
+
+    Returns:
+        - str: The password to be used for Showdown
+    """
     with open("secrets.json") as f:
         data = json.load(f)
         return data["password"]
