4e1f0c457afe52fbac37d8c62cb0af6f1c7ef3d9
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-06-15 19:33:39 +0200

    Add dropout

--- training.py
+++ training.py
@@ -12,30 +12,36 @@ import os
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.fc1 = nn.Linear(AIPlayer.N_F_TOTAL, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, AIPlayer.N_OUTPUTS)
+        self.net = nn.Sequential(
+            nn.Linear(AIPlayer.N_F_TOTAL, 128),
+            nn.Dropout(0.25),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.Dropout(0.25),
+            nn.ReLU(),
+            nn.Linear(64, AIPlayer.N_OUTPUTS),
+            nn.Softmax(dim=-1),  # Output layer with softmax activation
+        )
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
-        x = torch.softmax(x, dim=-1)  # Apply softmax to get probabilities
-        return x
+        return self.net(x)
 
 
 class CriticNetwork(nn.Module):
     def __init__(self):
         super(CriticNetwork, self).__init__()
-        self.fc1 = nn.Linear(AIPlayer.N_F_TOTAL, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 1)  # Output a single value for the state value
+        self.net = nn.Sequential(
+            nn.Linear(AIPlayer.N_F_TOTAL, 128),
+            nn.Dropout(0.25),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.Dropout(0.25),
+            nn.ReLU(),
+            nn.Linear(64, 1),  # Output a single value for the state value
+        )
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        return self.net(x)
 
 
 async def main():
