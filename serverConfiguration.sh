996654cae36b107fcb2daa12475a33570ec1902e
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-06-23 18:49:46 +0200

    More server configuration

--- serverConfiguration.sh
+++ serverConfiguration.sh
@@ -17,3 +17,19 @@ numCores=$(nproc)
 echo "Changing the number of workers to $numCores in $configFile"
 # Replace the line exports.workers = 1; with exports.workers = <numCores>;
 sed -i "s/^exports\.workers = [0-9]\+;/exports.workers = $numCores;/" "$configFile"
+
+echo "Changing the server to not report battles"
+# Replace the line exports.reportbattles = true; with exports.reportbattles = false;
+sed -i "s/^exports\.reportbattles = true;/exports.reportbattles = false;/" "$configFile"
+
+echo "Turning off the punishment monitor"
+# Replace exports.monitorminpunishments = *; with exports.monitorminpunishments = 0;
+sed -i "s/^exports\.monitorminpunishments = [0-9]\+;/exports.monitorminpunishments = 0;/" "$configFile"
+
+echo "Disabling throttling"
+# Replace exports.nothrottle = false; with exports.nothrottle = true;
+sed -i "s/^exports\.nothrottle = false;/exports.nothrottle = true;/" "$configFile"
+
+echo "Disabling IP checks"
+# Replace exports.noipchecks = false; with exports.noipchecks = true;
+sed -i "s/^exports\.noipchecks = false;/exports.noipchecks = true;/" "$configFile"
