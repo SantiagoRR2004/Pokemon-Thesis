38f898995ea8db7c54caca6ba265680eb8a1db04
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-08-15 12:13:45 +0200

    Move and improve move tester

---
+++ moves/test_Moves.py
@@ -0,0 +1,22 @@
+from pokemonFeatureEncoder import PokemonFeatureEncoder
+from poke_env.battle import Move
+import inspect
+import moves
+
+
+encoder = PokemonFeatureEncoder()
+
+
+# Get all classes defined in the module
+classes = inspect.getmembers(moves, inspect.isclass)
+
+
+for name, obj in classes:
+    if name != "AbstractMove":
+        for moveName in encoder.moveEncoder:
+            length = len(obj.getFeatures(Move(moveName, 9)))
+            assert (
+                obj.N_F_MOVE == length
+            ), f"Move {moveName} for class {name} {obj.N_F_MOVE} != {length}."
+
+        print(f"Class {name} is valid with {obj.N_F_MOVE} features.")
--- test_Moves.py
+++
@@ -1,9 +0,0 @@
-from pokemonFeatureEncoder import PokemonFeatureEncoder
-from moves import MoveS
-from poke_env.battle import Move
-
-
-encoder = PokemonFeatureEncoder()
-
-for moveName in encoder.moveEncoder:
-    MoveS.getFeatures(Move(moveName, 9))
