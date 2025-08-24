6b5874e31dcd7dcbe1d40c5199b839feba9ea6b0
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-08-24 22:05:27 +0200

    Add test (doesn't work)

---
+++ pokemons/test_Pokemons.py
@@ -0,0 +1,33 @@
+from pokemonFeatureEncoder import PokemonFeatureEncoder
+from poke_env.battle import Pokemon
+from moves import Move00
+import pokemons
+import inspect
+
+
+a = Pokemon(gen=9)
+
+encoder = PokemonFeatureEncoder()
+
+
+# Get all classes defined in the module
+classes = inspect.getmembers(pokemons, inspect.isclass)
+
+
+for name, obj in classes:
+    if name != "AbstractPokemon":
+        ins = obj(Move00)
+        for pokemonSpecies in encoder.formEncoder:
+            """
+            TODO Fix the pokemon problem
+            When the pokemon is created this way some attributes are missing
+            that are there when created normally
+            """
+            length = len(
+                ins.getFeatures(Pokemon(species=pokemonSpecies, gen=9), "dskjhfjhukds")
+            )
+            assert (
+                obj.N_F_POKEMON + 4 * (Move00.N_F_MOVE) == length
+            ), f"Move {pokemonSpecies} for class {name} {obj.N_F_POKEMON} != {length}."
+
+        print(f"Class {name} is valid with {ins.N_F_POKEMON} features.")
