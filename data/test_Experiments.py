3b5355ca867d470dd5ec59ece18f278aec06db2f
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-09-11 20:27:57 +0200

    More experiment data

--- data/test_Experiments.py
+++ data/test_Experiments.py
@@ -1,4 +1,9 @@
+from contextlib import redirect_stdout, redirect_stderr
 import pandas as pd
+import pokemons
+import players
+import moves
+import sys
 import os
 
 currentDirectory = os.path.dirname(os.path.abspath(__file__))
@@ -14,3 +19,26 @@ if data.duplicated(subset=subsetCols).any():
     print(data[data.duplicated(subset=subsetCols)])
 else:
     print("No duplicate rows found.")
+
+
+# Correctly calculate the nInputs column
+
+for index, row in data.iterrows():
+
+    try:
+
+        with open(os.devnull, "w") as fnull:
+            with redirect_stdout(fnull), redirect_stderr(fnull):
+
+                move = getattr(moves, row.move)()
+                pokemon = getattr(pokemons, row.pokemon)(move)
+                player = getattr(players, row.player)(
+                    network="BlaBlaBla", pokemonFeatureExtractor=pokemon
+                )
+
+        data.at[index, "nInputs"] = player.getNumberOfInputs()
+
+    except AttributeError as e:
+        continue
+
+data.to_csv(os.path.join(currentDirectory, "experiments.csv"), index=False)
