11efaecbcea3ca84600e4fb07795e81bc2ecf837
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2026-02-18 19:16:16 +0100

    Refactor graphAllExperiments

--- metricsLogger.py
+++ metricsLogger.py
@@ -55,6 +55,8 @@ class MetricsLogger:
     def relativeQuality(A: pd.DataFrame, B: pd.DataFrame) -> float:
         """
         Calculate the relative quality of two sets of metrics.
+        It will be a score between -1 and 1, where 1 means A is better than B,
+        -1 means B is better than A, and 0 means they are equal.
 
         Args:
             - A (pd.DataFrame): The first set of metrics.
@@ -63,7 +65,21 @@ class MetricsLogger:
         Returns:
             - float: The relative quality of A compared to B.
         """
-        return 0
+        # Find the common number of epochs
+        minEpochs = min(len(A["epoch"]), len(B["epoch"]))
+
+        # Reduce each dataframe if necessary
+        if len(A["epoch"]) > minEpochs:
+            A = A.iloc[:minEpochs].copy()
+        if len(B["epoch"]) > minEpochs:
+            B = B.iloc[:minEpochs].copy()
+
+        # Calculate the victory percentage score
+        weights = np.arange(1, minEpochs + 1) / minEpochs
+        diff = A["victoryPercentage"].to_numpy() - B["victoryPercentage"].to_numpy()
+        victoryPercentageScore = np.sum(weights * np.sign(diff)) / np.sum(weights)
+
+        return victoryPercentageScore
 
     def calculateComparisons(self) -> None:
         """
@@ -94,6 +110,156 @@ class MetricsLogger:
                 comparisonsDF.iloc[j, i] = -score
 
         self.comparisonsDF = comparisonsDF
+
+    def graphAllExperiments(self) -> None:
+        """
+        Graph the multiple metrics for the experiments.
+
+        Args:
+            - None
+
+        Returns:
+            - None
+        """
+        # Window size for smoothing
+        windowSize = 100
+
+        # Get the important files
+        top10 = (
+            self.comparisonsDF.sum(axis=1).sort_values(ascending=False).head(10).index
+        )
+        worst = (
+            self.comparisonsDF.sum(axis=1).sort_values(ascending=False).tail(1).index
+        )
+        files = {f: self.files[f] for f in top10.union(worst)}
+
+        # Plot the victory percentage
+        fig = go.Figure()
+        for name, df in files.items():
+            if "victoryPercentage" in df.columns:
+                smoothed = (
+                    df["victoryPercentage"]
+                    .rolling(window=windowSize, center=True)
+                    .mean()
+                )
+                fig.add_trace(
+                    go.Scatter(y=smoothed, mode="lines", name=name, hoverinfo="name+y")
+                )
+        fig.update_layout(
+            title="Victory Percentage Over Epochs",
+            xaxis_title="Epochs",
+            yaxis_title="Victory Percentage",
+            hovermode="closest",
+        )
+        fig.show()
+
+        # Plot the losses
+        fig = go.Figure()
+        for name, df in files.items():
+            if "actorLosses" in df.columns:
+                smoothed = (
+                    df["actorLosses"].rolling(window=windowSize, center=True).mean()
+                )
+                # Ensure losses are lower
+                if (
+                    not smoothed.dropna().empty
+                    and smoothed.dropna().iloc[-1] < smoothed.dropna().iloc[0]
+                ):
+                    fig.add_trace(
+                        go.Scatter(
+                            y=smoothed, mode="lines", name=name, hoverinfo="name+y"
+                        )
+                    )
+        fig.update_layout(
+            title="Actor Loss Over Epochs",
+            xaxis_title="Epochs",
+            yaxis_title="Loss",
+            hovermode="closest",
+        )
+        fig.show()
+
+        fig = go.Figure()
+        for name, df in files.items():
+            if "criticLosses" in df.columns:
+                smoothed = (
+                    df["criticLosses"].rolling(window=windowSize, center=True).mean()
+                )
+                # Ensure losses are lower
+                if (
+                    not smoothed.dropna().empty
+                    and smoothed.dropna().iloc[-1] < smoothed.dropna().iloc[0]
+                ):
+                    fig.add_trace(
+                        go.Scatter(
+                            y=smoothed, mode="lines", name=name, hoverinfo="name+y"
+                        )
+                    )
+        fig.update_layout(
+            title="Critic Loss Over Epochs",
+            xaxis_title="Epochs",
+            yaxis_title="Loss",
+            hovermode="closest",
+        )
+        fig.show()
+
+        # Plot the average rewards
+        fig = go.Figure()
+        for name, df in files.items():
+            if "averageRewards" in df.columns:
+                smoothed = (
+                    df["averageRewards"].rolling(window=windowSize, center=True).mean()
+                )
+                # Ensure rewards are higher
+                if (
+                    not smoothed.dropna().empty
+                    and smoothed.dropna().iloc[-1] > smoothed.dropna().iloc[0]
+                ):
+                    fig.add_trace(
+                        go.Scatter(
+                            y=smoothed, mode="lines", name=f"{name} Average Rewards"
+                        )
+                    )
+                    if "averageCriticRewards" in df.columns:
+                        smoothed = (
+                            df["averageCriticRewards"]
+                            .rolling(window=windowSize, center=True)
+                            .mean()
+                        )
+                        # Ensure rewards are higher
+                        if (
+                            not smoothed.dropna().empty
+                            and smoothed.dropna().iloc[-1] > smoothed.dropna().iloc[0]
+                        ):
+                            fig.add_trace(
+                                go.Scatter(
+                                    y=smoothed,
+                                    mode="lines",
+                                    name=f"{name} Average Critic Rewards",
+                                )
+                            )
+        fig.update_layout(
+            title="Average Rewards Over Epochs",
+            xaxis_title="Epochs",
+            yaxis_title="Average Rewards",
+            hovermode="closest",
+        )
+        fig.show()
+
+        # Plot the number of turns
+        fig = go.Figure()
+        for name, df in files.items():
+            if "nTurns" in df.columns:
+                smoothed = df["nTurns"].rolling(window=windowSize, center=True).mean()
+                fig.add_trace(
+                    go.Scatter(y=smoothed, mode="lines", name=name, hoverinfo="name+y")
+                )
+        fig.update_layout(
+            title="Number of Turns Over Epochs",
+            xaxis_title="Epochs",
+            yaxis_title="Number of Turns",
+            hovermode="closest",
+        )
+        fig.show()
 
 
 def saveData(
@@ -139,58 +305,6 @@ def saveData(
     df.to_parquet(os.path.join(dataDirectory, fileName + ".parquet"), index=False)
 
 
-def graphExperiment(fileName: str) -> None:
-    currentDirectory = os.path.dirname(os.path.abspath(__file__))
-    dataDirectory = os.path.join(currentDirectory, "data")
-
-    df = pd.read_parquet(os.path.join(dataDirectory, fileName))
-
-    # Plot the victory percentage
-    if "victoryPercentage" in df.columns:
-        plt.figure()
-        plt.plot(df["victoryPercentage"])
-        plt.xlabel("Epochs")
-        plt.ylabel("Victory Percentage")
-        plt.title("Victory Percentage Over Epochs")
-
-    # Plot the losses
-    if "actorLosses" in df.columns:
-        plt.figure()
-        plt.plot(df["actorLosses"])
-        plt.xlabel("Epochs")
-        plt.ylabel("Actor Loss")
-        plt.title("Actor Loss Over Epochs")
-
-    if "criticLosses" in df.columns:
-        plt.figure()
-        plt.plot(df["criticLosses"])
-        plt.xlabel("Epochs")
-        plt.ylabel("Critic Loss")
-        plt.title("Critic Loss Over Epochs")
-
-    # Plot the average rewards
-    if "averageRewards" in df.columns or "averageCriticRewards" in df.columns:
-        plt.figure()
-        if "averageRewards" in df.columns:
-            plt.plot(df["averageRewards"], label="Average Rewards")
-        if "averageCriticRewards" in df.columns:
-            plt.plot(df["averageCriticRewards"], label="Average Critic Rewards")
-        plt.xlabel("Epochs")
-        plt.ylabel("Average Rewards")
-        plt.title("Average Rewards Over Epochs")
-        plt.legend()
-
-    # Plot the number of turns
-    if "nTurns" in df.columns:
-        plt.figure()
-        plt.plot(df["nTurns"])
-        plt.xlabel("Epochs")
-        plt.ylabel("Number of Turns")
-        plt.title("Number of Turns Over Epochs")
-
-    plt.show()
-
-
 def sortLegend() -> None:
     """
     Sort the legend of the current plot by label name.
@@ -210,137 +324,6 @@ def sortLegend() -> None:
 
     # Apply sorted legend
     plt.legend(sorted_handles, sorted_labels)
-
-
-def graphAllExperiments(windowSize: int = 1) -> None:
-    currentDirectory = os.path.dirname(os.path.abspath(__file__))
-    dataDirectory = os.path.join(currentDirectory, "data")
-
-    # List all parquet files in the data directory
-    files = {
-        fileName[: -len(".parquet")]: pd.read_parquet(
-            os.path.join(dataDirectory, fileName)
-        )
-        for fileName in os.listdir(dataDirectory)
-        if fileName.endswith(".parquet")
-    }
-    files = dict(sorted(files.items(), key=lambda item: item[0]))  # sort by name
-
-    # Plot the victory percentage
-    fig = go.Figure()
-    for name, df in files.items():
-        if "victoryPercentage" in df.columns:
-            smoothed = (
-                df["victoryPercentage"].rolling(window=windowSize, center=True).mean()
-            )
-            fig.add_trace(
-                go.Scatter(y=smoothed, mode="lines", name=name, hoverinfo="name+y")
-            )
-    fig.update_layout(
-        title="Victory Percentage Over Epochs",
-        xaxis_title="Epochs",
-        yaxis_title="Victory Percentage",
-        hovermode="closest",
-    )
-    fig.show()
-
-    # Plot the losses
-    fig = go.Figure()
-    for name, df in files.items():
-        if "actorLosses" in df.columns:
-            smoothed = df["actorLosses"].rolling(window=windowSize, center=True).mean()
-            # Ensure losses are lower
-            if (
-                not smoothed.dropna().empty
-                and smoothed.dropna().iloc[-1] < smoothed.dropna().iloc[0]
-            ):
-                fig.add_trace(
-                    go.Scatter(y=smoothed, mode="lines", name=name, hoverinfo="name+y")
-                )
-    fig.update_layout(
-        title="Actor Loss Over Epochs",
-        xaxis_title="Epochs",
-        yaxis_title="Loss",
-        hovermode="closest",
-    )
-    fig.show()
-
-    fig = go.Figure()
-    for name, df in files.items():
-        if "criticLosses" in df.columns:
-            smoothed = df["criticLosses"].rolling(window=windowSize, center=True).mean()
-            # Ensure losses are lower
-            if (
-                not smoothed.dropna().empty
-                and smoothed.dropna().iloc[-1] < smoothed.dropna().iloc[0]
-            ):
-                fig.add_trace(
-                    go.Scatter(y=smoothed, mode="lines", name=name, hoverinfo="name+y")
-                )
-    fig.update_layout(
-        title="Critic Loss Over Epochs",
-        xaxis_title="Epochs",
-        yaxis_title="Loss",
-        hovermode="closest",
-    )
-    fig.show()
-
-    # Plot the average rewards
-    fig = go.Figure()
-    for name, df in files.items():
-        if "averageRewards" in df.columns:
-            smoothed = (
-                df["averageRewards"].rolling(window=windowSize, center=True).mean()
-            )
-            # Ensure rewards are higher
-            if (
-                not smoothed.dropna().empty
-                and smoothed.dropna().iloc[-1] > smoothed.dropna().iloc[0]
-            ):
-                fig.add_trace(
-                    go.Scatter(y=smoothed, mode="lines", name=f"{name} Average Rewards")
-                )
-                if "averageCriticRewards" in df.columns:
-                    smoothed = (
-                        df["averageCriticRewards"]
-                        .rolling(window=windowSize, center=True)
-                        .mean()
-                    )
-                    # Ensure rewards are higher
-                    if (
-                        not smoothed.dropna().empty
-                        and smoothed.dropna().iloc[-1] > smoothed.dropna().iloc[0]
-                    ):
-                        fig.add_trace(
-                            go.Scatter(
-                                y=smoothed,
-                                mode="lines",
-                                name=f"{name} Average Critic Rewards",
-                            )
-                        )
-    fig.update_layout(
-        title="Average Rewards Over Epochs",
-        xaxis_title="Epochs",
-        yaxis_title="Average Rewards",
-        hovermode="closest",
-    )
-    fig.show()
-
-    # Plot the number of turns
-    fig = go.Figure()
-    for name, df in files.items():
-        if "nTurns" in df.columns:
-            smoothed = df["nTurns"].rolling(window=windowSize, center=True).mean()
-            fig.add_trace(
-                go.Scatter(y=smoothed, mode="lines", name=name, hoverinfo="name+y")
-            )
-    fig.update_layout(
-        title="Number of Turns Over Epochs",
-        xaxis_title="Epochs",
-        yaxis_title="Number of Turns",
-        hovermode="closest",
-    )
-    fig.show()
 
 
 def graphVictoryPercentage() -> None:
@@ -409,5 +392,6 @@ def graphVictoryPercentage() -> None:
 
 
 if __name__ == "__main__":
-    graphAllExperiments(100)
+    logger = MetricsLogger()
+    logger.graphAllExperiments()
     graphVictoryPercentage()
