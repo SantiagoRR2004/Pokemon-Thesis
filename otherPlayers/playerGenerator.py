8c12c95fcd05e88403fa6b3150539ad2764569a8
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2026-03-05 12:44:13 +0100

    Add getPlayerExperiment

--- otherPlayers/playerGenerator.py
+++ otherPlayers/playerGenerator.py
@@ -1,5 +1,14 @@
-from poke_env.player import RandomPlayer
+from poke_env.ps_client.server_configuration import ServerConfiguration
 from otherPlayers.maxDamagePlayer import MaxRandomDamagePlayer
+from players.AbstractAIPlayer import AbstractAIPlayer
+from poke_env.player import RandomPlayer
+import pandas as pd
+import pokemons
+import players
+import actors
+import torch
+import moves
+import os
 
 
 def getRandomPlayer(args: dict = {}) -> RandomPlayer:
@@ -26,3 +35,55 @@ def getRandomMaxDamagePlayer(args: dict = {}) -> MaxRandomDamagePlayer:
         - MaxDamagePlayer: An instance of MaxDamagePlayer.
     """
     return MaxRandomDamagePlayer(**args)
+
+
+def getPlayerExperiment(
+    n: int, serverConfig: ServerConfiguration, concurrentBattles: int = 100
+) -> AbstractAIPlayer:
+    """
+    This function returns a player for a given experiment number.
+
+    Args:
+        - n (int): The experiment number for which to return the player.
+        - serverConfig (ServerConfiguration): The server configuration to use for the player.
+        - concurrentBattles (int): The number of concurrent battles the player
+            allows (default is 100).
+
+    Returns:
+        - AbstractAIPlayer: An instance of AbstractAIPlayer corresponding to the experiment number.
+    """
+    currentDirectory = os.path.dirname(os.path.abspath(__file__))
+    dataPath = os.path.join(os.path.dirname(currentDirectory), "data")
+    modelFile = os.path.join(dataPath, f"experiment{n}Actor.pth")
+    dataFile = os.path.join(dataPath, f"experiments.csv")
+
+    # Check if the model file exists
+    if not os.path.exists(modelFile):
+        raise ValueError(f"Model file {modelFile} does not exist.")
+
+    allData = pd.read_csv(dataFile)
+
+    # Check if the experiment number exists in the data
+    if f"experiment{n}" not in allData["fileName"].values:
+        raise ValueError(f"Experiment number {n} does not exist in the data.")
+
+    data = allData[allData["fileName"] == f"experiment{n}"].iloc[0]
+
+    player: AbstractAIPlayer = getattr(players, data["player"])(
+        network="BlaBlaBla",
+        battle_format="gen9randombattle",
+        pokemonFeatureExtractor=getattr(pokemons, data["pokemon"])(
+            getattr(moves, data["move"])
+        ),
+        server_configuration=serverConfig,
+        max_concurrent_battles=concurrentBattles,
+    )
+
+    actor: actors.AbstractActor = getattr(actors, data["actor"])(player)
+    actor.load_state_dict(torch.load(modelFile))
+    actor.eval()
+
+    # Set the correct network for the player
+    player.neuralNetwork = actor
+
+    return player
