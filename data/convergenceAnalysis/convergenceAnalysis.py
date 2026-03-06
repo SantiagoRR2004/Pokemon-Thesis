f6ed737c86092e3c586e0695f2444098be82f0c8
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2026-03-06 11:17:46 +0100

    Add convergence analysis

---
+++ data/convergenceAnalysis/convergenceAnalysis.py
@@ -0,0 +1,120 @@
+from poke_env.ps_client.server_configuration import ServerConfiguration
+import serverControl
+import otherPlayers
+import asyncio
+import json
+import os
+
+
+class RandomVictoryPercentage:
+    """
+    In theory if a player plays against himself, he should
+    only win 50% of the time. This class is created to calculate
+    the number of battles needed to be sure that the victory
+    percentage is within a percentage (1%)
+    """
+
+    def __init__(self, decimalPrecision: int = 2) -> None:
+        """
+        Initializes the random victory percentage and also
+        calculates the percentage needed.
+
+        Args:
+            - decimalPrecision (int): The number of decimal places to round the percentage to.
+
+        Returns:
+            - None
+        """
+        self.server = ServerConfiguration(
+            f"ws://localhost:{int(os.getenv('SERVER_PORT'))}/showdown/websocket",
+            "https://play.pokemonshowdown.com/action.php?",
+        )
+        self.decimalPrecision = decimalPrecision
+
+        self.currentDirectory = os.path.dirname(os.path.abspath(__file__))
+        self.dataFile = os.path.join(self.currentDirectory, "random.json")
+
+        # Load the data from the JSON file
+        if os.path.exists(self.dataFile):
+            with open(self.dataFile, "r") as f:
+                self.data = json.load(f)
+        else:
+            self.data = []
+
+        # Calculate the percentage needed
+        self.calculatePercentage()
+
+        # End the server process
+        serverControl.endProcess(self.p)
+        self.p.wait()
+
+        # Save the data to the JSON file
+        with open(self.dataFile, "w") as f:
+            json.dump(self.data, f, indent=4)
+            f.write("\n")
+
+    def resetServer(self) -> None:
+        """
+        Resets the server by ending the current process and starting a new one.
+        Also creates two new random players.
+
+        Args:
+            - None
+
+        Returns:
+            - None
+        """
+        if getattr(self, "p", None) is not None:
+            serverControl.endProcess(self.p)
+            self.p.wait()
+
+        self.p = serverControl.startServer()
+
+        # Create the players
+        self.player1 = otherPlayers.getRandomPlayer(
+            args={"server_configuration": self.server}
+        )
+        self.player2 = otherPlayers.getRandomPlayer(
+            args={"server_configuration": self.server}
+        )
+
+    def calculatePercentage(self) -> None:
+        """
+        Calculates the percentage needed by playing battles between two random players
+        until the percentage is within the desired range.
+
+        Args:
+            - None
+
+        Returns:
+            - None
+        """
+        # Add one extra row
+        self.data.append([])
+
+        serverCounter = 0
+
+        for row in self.data:
+
+            percentage = sum(row) / len(row) if len(row) > 0 else 0
+
+            while len(row) < 100 or round(percentage, self.decimalPrecision) != 0.5:
+
+                # Restart server every 100 battles
+                if serverCounter % 100 == 0:
+                    self.resetServer()
+                    serverCounter = 0
+
+                # Play a single game
+                asyncio.run(self.player1.battle_against(self.player2, n_battles=1))
+                row.append(int(self.player1.win_rate))
+
+                # Prepare for next iteration
+                percentage = sum(row) / len(row)
+                serverCounter += 1
+                self.player1.reset_battles()
+                self.player2.reset_battles()
+
+
+if __name__ == "__main__":
+    RandomVictoryPercentage()
